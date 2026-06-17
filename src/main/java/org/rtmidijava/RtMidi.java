c41aa4ed893e2ae74eb23ab5aa46921e654cde41
Ludovic Champenois <ludo@google.com>
2026-06-17 08:39:32 -0700

    Release 1.0.5

--- src/main/java/org/rtmidijava/RtMidi.java
+++ src/main/java/org/rtmidijava/RtMidi.java
@@ -10,7 +10,7 @@ import java.util.List;
 public abstract class RtMidi {
 
     /** Version of the RtMidiJava port. */
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     /**
      * Enumeration of supported native MIDI APIs.
